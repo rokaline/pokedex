510ca42aebb05a7dbe903dd30c1e6e405e034ca1
rokaline <124616570+rokaline@users.noreply.github.com>
2024-06-17 00:38:43 +0200

    17.06 00.38

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
 
                 <!-- Navigation Links (dans la nav de la partie user)-->
                 {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                  <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div> --}}
@@ -34,7 +34,7 @@
                         <a class="font-bold hover:text-yellow-600 transition" href="{{ route('homepage.pokemons.index') }}">Tableau des Pokémon (visible apd dash)</a>
                     </div>
                 </div>
-              
+
 
 
 
