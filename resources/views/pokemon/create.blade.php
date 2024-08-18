84fecb5bb26492b494674cf3c508176df3b440a5
rokaline <124616570+rokaline@users.noreply.github.com>
2024-08-18 18:56:19 +0200

    final

--- resources/views/pokemon/create.blade.php
+++ resources/views/pokemon/create.blade.php
@@ -15,42 +15,42 @@
                 @csrf
                 <!-- Pokémon Name -->
                 <div>
-                    <x-input-label for="nom" :value="__('Nom Pokémon')" />
-                    <x-text-input id="nom" class="block mt-1 w-full" type="text" name="nom" :value="old('nom')" required autofocus />
+                    <x-input-label for="nom" :value="__('Nom Pokémon')" class="text-red-300"/>
+                    <x-text-input id="nom" class="block mt-1 w-full bg-gray-700 border-red-500 text-white" type="text" name="nom" :value="old('nom')" required autofocus />
                     <x-input-error :messages="$errors->get('nom')" class="mt-2" />
                 </div>
 
                 <!-- Pokémon Image -->
                 <div>
-                    <x-input-label for="img_path" :value="__('Image Pokémon')" />
-                    <x-text-input id="img_path" class="block mt-1 w-full" type="file" name="img_path" required />
+                    <x-input-label for="img_path" :value="__('Image Pokémon')" class="text-red-300"/>
+                    <x-text-input id="img_path" class="block mt-1 w-full bg-gray-700 border-red-500 text-white"  type="file" name="img_path" required />
                     <x-input-error :messages="$errors->get('img_path')" class="mt-2" />
                 </div>
 
                 <!-- Pokémon PV -->
                 <div>
-                    <x-input-label for="pv" :value="__('PV')" />
-                    <x-text-input id="pv" class="block mt-1 w-full" type="number" name="pv" :value="old('pv')" required />
+                    <x-input-label for="pv" :value="__('PV')" class="text-red-300"/>
+                    <x-text-input id="pv" class="block mt-1 w-full bg-gray-700 border-red-500 text-white"  type="number" name="pv" :value="old('pv')" required />
                     <x-input-error :messages="$errors->get('pv')" class="mt-2" />
                 </div>
 
                 <!-- Pokémon Weight -->
                 <div>
-                    <x-input-label for="poids" :value="__('Poids')" />
-                    <x-text-input id="poids" class="block mt-1 w-full" type="number" step="0.1" name="poids" :value="old('poids')" required />
+                    <x-input-label for="poids" :value="__('Poids')" class="text-red-300"/>
+                    <x-text-input id="poids" class="block mt-1 w-full bg-gray-700 border-red-500 text-white" type="number" step="0.1" name="poids" :value="old('poids')" required />
                     <x-input-error :messages="$errors->get('poids')" class="mt-2" />
                 </div>
 
                 <!-- Pokémon Height -->
                 <div>
-                    <x-input-label for="taille" :value="__('Taille')" />
-                    <x-text-input id="taille" class="block mt-1 w-full" type="number" step="0.1" name="taille" :value="old('taille')" required />
+                    <x-input-label for="taille" :value="__('Taille')" class="text-red-300"/>
+                    <x-text-input id="taille" class="block mt-1 w-full bg-gray-700 border-red-500 text-white" type="number" step="0.1" name="taille" :value="old('taille')" required />
                     <x-input-error :messages="$errors->get('taille')" class="mt-2" />
                 </div>
 
 
 
-               
+
 
                 <!-- Submit Button -->
                 <div class="flex justify-end">
--- resources/views/pokemon/edit.blade.php
+++ resources/views/pokemon/edit.blade.php
@@ -8,7 +8,7 @@
             <div class="bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <div class="flex justify-between mt-8">
                     <div class="text-2xl text-white">
-                        Modifier un Type
+                        Modifier un Pokemon
                     </div>
                 </div>
 
@@ -20,36 +20,36 @@
                     <!-- Pokémon Name -->
                     <div>
                         <x-input-label for="nom" :value="__('Nom Pokémon')"  class="text-red-300"/>
-                        <x-text-input id="nom" class="block mt-1 w-full" type="text" name="nom" :value="old('nom', $pokemon->nom)" required autofocus />
-                        <x-input-error :messages="$errors->get('nom')" class="mt-2" />
+                        <x-text-input id="nom" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white" type="text" name="nom" :value="old('nom', $pokemon->nom)" required autofocus />
+                        <x-input-error :messages="$errors->get('nom')" class="mt-2 text-yellow-300" />
                     </div>
 
                     <!-- Pokémon Image -->
                     <div>
                         <x-input-label for="img_path" :value="__('Image Pokémon')"  class="text-red-300"/>
-                        <x-text-input id="img_path" class="block mt-1 w-full" type="file" name="img_path" />
-                        <x-input-error :messages="$errors->get('img_path')" class="mt-2" />
+                        <x-text-input id="img_path" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white" type="file" name="img_path" />
+                        <x-input-error :messages="$errors->get('img_path')" class="mt-2 text-yellow-300" />
                     </div>
 
                     <!-- Pokémon PV -->
                     <div>
                         <x-input-label for="pv" :value="__('PV')"  class="text-red-300"/>
-                        <x-text-input id="pv" class="block mt-1 w-full" type="number" name="pv" :value="old('pv', $pokemon->pv)" required />
-                        <x-input-error :messages="$errors->get('pv')" class="mt-2" />
+                        <x-text-input id="pv" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white" type="number" name="pv" :value="old('pv', $pokemon->pv)" required />
+                        <x-input-error :messages="$errors->get('pv')" class="mt-2 text-yellow-300" />
                     </div>
 
                     <!-- Pokémon Weight -->
                     <div>
                         <x-input-label for="poids" :value="__('Poids')"  class="text-red-300"/>
-                        <x-text-input id="poids" class="block mt-1 w-full" type="number" step="0.1" name="poids" :value="old('poids', $pokemon->poids)" required />
-                        <x-input-error :messages="$errors->get('poids')" class="mt-2" />
+                        <x-text-input id="poids" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white" type="number" step="0.1" name="poids" :value="old('poids', $pokemon->poids)" required />
+                        <x-input-error :messages="$errors->get('poids')" class="mt-2 text-yellow-300" />
                     </div>
 
                     <!-- Pokémon Height -->
                     <div>
                         <x-input-label for="taille" :value="__('Taille')"  class="text-red-300"/>
-                        <x-text-input id="taille" class="block mt-1 w-full" type="number" step="0.1" name="taille" :value="old('taille', $pokemon->taille)" required />
-                        <x-input-error :messages="$errors->get('taille')" class="mt-2" />
+                        <x-text-input id="taille" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white" type="number" step="0.1" name="taille" :value="old('taille', $pokemon->taille)" required />
+                        <x-input-error :messages="$errors->get('taille')" class="mt-2 text-yellow-300" />
                     </div>
 
 
@@ -58,7 +58,7 @@
                     <!-- Type Obligatoire -->
                     <div>
                         <x-input-label for="type_obligatoire" :value="__('Type Obligatoire')"  class="text-red-300"/>
-                        <select name="type_obligatoire" id="type_obligatoire" class="border border-red-500 rounded shadow px-4 py-2 w-full">
+                        <select name="type_obligatoire" id="type_obligatoire" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white">
                             <option value="">Sélectionnez un type obligatoire</option>
                             @foreach($types as $type)
                                 <option value="{{ $type->id }}" {{ old('type_obligatoire', $pokemon->types->first()?->id) == $type->id ? 'selected' : '' }}>
@@ -66,14 +66,14 @@
                                 </option>
                             @endforeach
                         </select>
-                        <x-input-error :messages="$errors->get('type_obligatoire')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('type_obligatoire')" class="mt-2 text-yellow-300" />
                     </div>
 
 
                     <!-- Attaque Obligatoire -->
                     <div>
                         <x-input-label for="attaque_obligatoire" :value="__('Attaque Obligatoire')"  class="text-red-300"/>
-                        <select name="attaque_obligatoire" id="attaque_obligatoire" class="border border-red-500 rounded shadow px-4 py-2 w-full">
+                        <select name="attaque_obligatoire" id="attaque_obligatoire" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white">
                             <option value="">Sélectionnez une attaque obligatoire</option>
                             @foreach($attaques as $attaque)
                                 <option value="{{ $attaque->id }}" {{ old('attaque_obligatoire', $pokemon->attaques->first()?->id) == $attaque->id ? 'selected' : '' }}>
@@ -81,7 +81,7 @@
                                 </option>
                             @endforeach
                         </select>
-                        <x-input-error :messages="$errors->get('attaque_obligatoire')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('attaque_obligatoire')" class="mt-2 text-yellow-300" />
                     </div>
 
 
@@ -92,29 +92,29 @@
                     <!-- NOM Type Optionnel -->
                     <div>
                         <x-input-label for="type_optionnel" :value="__('Type Optionnel')"  class="text-red-300"/>
-                        <select name="type_optionnel" id="type_optionnel" class="border border-red-500 rounded shadow px-4 py-2 w-full">
-                            <option value="">Sélectionnez un type optionnel</option>
+                        <select name="type_optionnel" id="type_optionnel" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white">
+                            <option value="">Le deuxième type doit-être different du premier.</option>
                             @foreach($types as $type)
                                 <option value="{{ $type->id }}" {{ old('type_optionnel', $pokemon->types->count() > 1 ? $pokemon->types->get(1)->id : null) == $type->id ? 'selected' : '' }}>
                                     {{ $type->nom }}
                                 </option>
                             @endforeach
                         </select>
-                        <x-input-error :messages="$errors->get('type_optionnel')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('type_optionnel')" class="mt-2 text-yellow-300" />
                     </div>
 
                     <!-- Attaque Optionnelle -->
                     <div>
                         <x-input-label for="attaque_optionnelle" :value="__('Attaque Optionnelle')"  class="text-red-300"/>
-                        <select name="attaque_optionnelle" id="attaque_optionnelle" class="border border-red-500 rounded shadow px-4 py-2 w-full">
-                            <option value="">Sélectionnez une attaque optionnelle</option>
+                        <select name="attaque_optionnelle" id="attaque_optionnelle" class="block mt-1 w-full bg-gray-700 border border-red-500 text-white">
+                            <option value="">La deuxième attaquedoit-être different du premier.</option>
                             @foreach($attaques as $attaque)
                                 <option value="{{ $attaque->id }}" {{ old('attaque_optionnelle', $pokemon->attaques->count() > 1 ? $pokemon->attaques->get(1)->id : null) == $attaque->id ? 'selected' : '' }}>
                                     {{ $attaque->nom }}
                                 </option>
                             @endforeach
                         </select>
-                        <x-input-error :messages="$errors->get('attaque_optionnelle')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('attaque_optionnelle')" class="mt-2 text-yellow-300" />
                     </div>
 
 
